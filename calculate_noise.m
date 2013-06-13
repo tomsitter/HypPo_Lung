36400b2bc2a44a3df852d6ec58c11421570b8f24
Tom Sitter <tpsitter@lakeheadu.ca>
2013-06-13 17:21:17 -0400

    Changed beta value of noise claculation from 0.5 to 0.1 for better fitting

--- calculate_noise.m
+++ calculate_noise.m
@@ -16,7 +16,7 @@ h = hist(noise_roi, x);
     
     % User selects initial parameter for regression
     % (Usual values for beta: 0.1, 0.01, 0.001)
-    beta_value = 0.05; %input('Input initial parameter beta for regression: ');
+    beta_value = 0.01; %input('Input initial parameter beta for regression: ');
     beta = [beta_value, beta_value, beta_value];
     
     % Determines best-fit Rayleigh model
