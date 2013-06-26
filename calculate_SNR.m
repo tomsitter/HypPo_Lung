3a1716471b727ecb894bd36406aa7a5eaa8f4fa1
Steven E <smengler3@gmail.com>
2013-06-26 09:41:22 -0400

    Removed variable print in calculateSNR.m

--- calculate_SNR.m
+++ calculate_SNR.m
@@ -6,7 +6,6 @@ function SNR = calculate_SNR(signal_masks, noise_masks, data)
 		dataSelected = sliceData(signal_masks(:,:,a)==1);
 		S(a) = mean(dataSelected);
 	end
-	S
 	%
 	Noise = [];
 	for a=1:size(data,3)
@@ -14,7 +13,6 @@ function SNR = calculate_SNR(signal_masks, noise_masks, data)
 		dataSelected = sliceData(noise_masks(:,:,a)==1);
 		Noise(a) = std(dataSelected);
 	end
-	Noise
 	%
 	SNR=S./Noise;
 end
