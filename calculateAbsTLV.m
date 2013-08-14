ef2cd5505dfe495fc104ced19c3fd2c34dfa4b26
Steven E <smengler3@gmail.com>
2013-08-13 23:25:50 -0400

    Fixed an error where when calculating absolute volume, "signal/max signal" could be greater than 1, meaning the voxel had more than 100% gas. It now limits the ratio to 100%.

--- calculateAbsTLV.m
+++ calculateAbsTLV.m
@@ -3,5 +3,7 @@ function aTLV = calculateAbsTLV(maxSignalH, bodyParms, bodyImageSlice, bodyMaskS
 	voxelVolume = (bodyParms.fov(3)/bodyParms.reconstruction_res(2))*(bodyParms.fov(2)/bodyParms.reconstruction_res(1))*(bodyParms.slice_thickness+bodyParms.slice_gap);
 	voxelVolume = voxelVolume/10^3;
 	% converting to mL
-	aTLV = sum(sum((maxSignalH-double(bodyImageSlice(logical(bodyMaskSlice))))/maxSignalH*voxelVolume));
+	tlvRatios = (maxSignalH-double(bodyImageSlice(logical(bodyMaskSlice))))/maxSignalH;
+	tlvRatios(tlvRatios>1) = 1;
+	aTLV = sum(sum(tlvRatios))*voxelVolume;
 end
--- calculateAbsVLV.m
+++ calculateAbsVLV.m
@@ -2,5 +2,7 @@ function aVLV = calculateAbsVLV(signalFull, lungParms, lungImageSlice, lungMaskS
 	voxelVolume = (lungParms.fov(3)/lungParms.reconstruction_res(2))*(lungParms.fov(2)/lungParms.reconstruction_res(1))*(lungParms.slice_thickness+lungParms.slice_gap);
 	voxelVolume = voxelVolume/10^3;
 	% converting to mL
-	aVLV = sum(sum(lungImageSlice(logical(lungMaskSlice))))/signalFull*voxelVolume;
+	vlvRatios = lungImageSlice(logical(lungMaskSlice))/signalFull;
+	vlvRatios(vlvRatios>1) = 1;
+	aVLV = sum(sum(vlvRatios))*voxelVolume;
 end
